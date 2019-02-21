c534d1f683a60be656284815c98d505b652f9615
joel <jlmukono@gamil.com>
2019-02-21 14:47:25 +0300

    add more questions and add columns to seperate them

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,16 @@ $(document).ready(function(){
    var language = $("input:radio[name=language]:checked").val();
    var keyword = $("input:radio[name=keyword]:checked").val();
    var tag = $("input:radio[name=tag]:checked").val();
+   var popUp = $("input:radio[name=popUp]:checked").val();
+
+   if (language == "scripting" && keyword == "var" && tag == "script" && popUp =="alert()"){
+     alert("correct");
+
+   }
+
+   else{
+     alert("wrong");
+   }
    var points=0;
 
 

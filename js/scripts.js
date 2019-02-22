5c6ad0b6d23bb44168530c09724f4fac9e5a2e8f
joel <jlmukono@gamil.com>
2019-02-22 15:42:44 +0300

    add javascript varriables to collect values from html

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,19 @@
 $(document).ready(function(){
-  $("#myForm").submit(function(){
+  $("#myForm").submit(function(event){
+   event.preventDefault();
    var language = $("input:radio[name=language]:checked").val();
    var keyword = $("input:radio[name=keyword]:checked").val();
    var tag = $("input:radio[name=tag]:checked").val();
    var popUp = $("input:radio[name=popUp]:checked").val();
    var callFunction = $("input:radio[name=callFunction]:checked").val();
    var method = $("input:radio[name=method]:checked").val();
-   var reverse = $("input:radio[name=reverse:checked]").val();
-   var statement = $("input:radio[name=statement:checked]").val();
+   var back = $("input:radio[name=back]:checked").val();
+   var statement = $("input:radio[name=statement]:checked").val();
 
-   if (language == "scripting" && keyword == "var" && tag == "script" && popUp =="alert()" && callFunction=="function()" && method == "pop()" && reverse == "reverse()" && statement == "if()"){
-       alert("correct");
 
-    }
-   else{
-     alert("wrong");
+   if(language == ){
+
    }
-
 
 
   });

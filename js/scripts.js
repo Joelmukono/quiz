6f82364d1fc83d245143de957babd30812d674c5
joel <jlmukono@gamil.com>
2019-02-22 16:27:16 +0300

    add if statement to check if all answers are correct

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,23 @@
 $(document).ready(function(){
   $("#myForm").submit(function(event){
    event.preventDefault();
-   var language = $("input:radio[name=language]:checked").val();
-   var keyword = $("input:radio[name=keyword]:checked").val();
-   var tag = $("input:radio[name=tag]:checked").val();
-   var popUp = $("input:radio[name=popUp]:checked").val();
-   var callFunction = $("input:radio[name=callFunction]:checked").val();
-   var method = $("input:radio[name=method]:checked").val();
-   var back = $("input:radio[name=back]:checked").val();
-   var statement = $("input:radio[name=statement]:checked").val();
+   var language = $("input:radio[name=language]:checked").val();//scripting
+   var keyword = $("input:radio[name=keyword]:checked").val();//var
+   var tag = $("input:radio[name=tag]:checked").val();//script
+   var popUp = $("input:radio[name=popUp]:checked").val();//alert
+   var callFunction = $("input:radio[name=callFunction]:checked").val();//function()
+   var method = $("input:radio[name=method]:checked").val();//pop
+   var back = $("input:radio[name=back]:checked").val();//reverse()
+   var statement = $("input:radio[name=statement]:checked").val();//if()
 
+  answers=["scripting","var","script","alert","function","pop","reverse","if"];
 
-   if(language == ){
+   if(language == answers[0] && keyword == answers[1] &&
+      tag == answers[2] && popUp == answers[3] &&
+      callFunction == answers[4] && method == answers[5] && back == answers[6]  &&
+      statement == answers[7]){
+
+         alert("you got all correct");
 
    }
 

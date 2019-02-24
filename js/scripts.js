fd921a70f4ec48c0a02ad28bd0f958674e2bef5c
joel <jlmukono@gamil.com>
2019-02-24 15:06:19 +0300

    create array to store input elements

--- js/scripts.js
+++ js/scripts.js
@@ -9,49 +9,19 @@ $(document).ready(function(){
    var method = $("input:radio[name=method]:checked").val();//pop
    var back = $("input:radio[name=back]:checked").val();//reverse()
    var statement = $("input:radio[name=statement]:checked").val();//if()
+   var score = 0;
+   var answers=["scripting","var","script","alert","function","pop","reverse","if"];
 
-  answers=["scripting","var","script","alert","function","pop","reverse","if"];
+   //if(language == answers[0]){
+       //alert(score+=2);
 
-   if(language == answers[0] && keyword == answers[1] &&
-      tag == answers[2] && popUp == answers[3] &&
-      callFunction == answers[4] && method == answers[5] && back == answers[6]  &&
-      statement == answers[7]){
+   //}else if(language != answers[0]){
+        //alert(score=0);
+   //}
+   var inputs=[language,keyword,tag,popUp,callFunction,method,back,statement];
 
-         alert("you got all correct");
 
-   }else if(language != answers[0] && keyword == answers[1] &&
-           tag == answers[2] && popUp == answers[3] &&
-           callFunction == answers[4] && method == answers[5] && back == answers[6]  &&
-           statement == answers[7]){
-
-              alert("you got 7 correct")
-
-   }else if(language == answers[0] && keyword != answers[1] &&
-           tag == answers[2] && popUp == answers[3] &&
-           callFunction == answers[4] && method == answers[5] && back == answers[6]  &&
-           statement == answers[7]){
-
-              alert("you got 7 correct")
-
-   }else if(language == answers[0] && keyword == answers[1] &&
-            tag != answers[2] && popUp == answers[3] &&
-            callFunction == answers[4] && method == answers[5] && back == answers[6]  &&
-            statement == answers[7]){
-
-              alert("you got 7 correct")
-
-   }else if(language == answers[0] && keyword == answers[1] &&
-            tag == answers[2] && popUp != answers[3] &&
-            callFunction == answers[4] && method == answers[5] && back == answers[6]  &&
-            statement == answers[7]){
-
-              alert("you got 7 correct")
-
-   }
+   });
 
 
   });
-
-
-
-});
